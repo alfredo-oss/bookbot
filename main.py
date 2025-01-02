e38ee49e888f79b34221b1838a50d574d6d91dcb
alfredo-oss <alfredo.derodt@gmail.com>
2025-01-02 17:26:12 -0300

    B: added character count function

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 def wordCount(s: str) -> int:
     s_list = s.split()  
-    return len(s_list)
+    return len(s_list), s_list
+
+def characterCount(s_list: list[str]) -> int:
+    s = "".join(s_list)
+    count = {}
+    for c in s:
+        count[c.lower()] = 1 + count.get(c.lower(), 0)
+    for key, value in count.items():
+        print(f"Character: {key} | {value}")    
+    
+
 path_to_file = "books/frankestein.txt"
 
 with open(path_to_file) as f:
     file_contents = f.read()
-
-print(wordCount(file_contents))
+wc, s_list = wordCount(file_contents)
+characterCount(s_list)
