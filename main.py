9c19e62a90bb07596b1daba37da9f3312d3a11f8
alfredo-oss <alfredo.derodt@gmail.com>
2025-01-02 20:33:02 -0300

    C: added a reporting functionality and dict ordering function

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ def wordCount(s: str) -> int:
     s_list = s.split()  
     return len(s_list), s_list
 
+def sortDictDescending(numDict: dict) -> dict:
+    return dict(sorted(numDict.items(), key=lambda item: item[1], reverse=True))
+
 def characterCount(s_list: list[str]) -> int:
     s = "".join(s_list)
     count = {}
     for c in s:
         count[c.lower()] = 1 + count.get(c.lower(), 0)
-    for key, value in count.items():
-        print(f"Character: {key} | {value}")    
+    revCount = sortDictDescending(count)
+    for key, value in revCount.items():
+        print(f"Character: '{key}' appears {value} times ")    
     
 
 path_to_file = "books/frankestein.txt"
@@ -16,4 +20,9 @@ path_to_file = "books/frankestein.txt"
 with open(path_to_file) as f:
     file_contents = f.read()
 wc, s_list = wordCount(file_contents)
+
+print("\n")
+print(f"---------------------- Begin: {path_to_file} Report -----------------------")
+print(f"{wc} words found in the document\n\n")
 characterCount(s_list)
+print("----------------------- End Report ------------------------------------------")
